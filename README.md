8c16ae197a9da396d3e6607e17a85f1252420709
wandergis <763393390@qq.com>
2016-05-03 18:21:32 +0800

    添加数据和在线预览

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # putian-hospitals
 莆田系医院数据及其经纬度坐标，包括geojson和shp文件。
+
+## 数据来源
+
+-[凤凰网的数据](http://news.ifeng.com/mainland/special/ptxyy/)
+
+- 解密其`main2.min.js`文件，得到了hospitals的数组，使用python转成json数据。
+
+## 坐标获取
+
+- 使用node调用高德的geocode api解析。
+
+
---
+++ getLocation.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var _ = require('lodash');
+var async = require('async');
+var request = require('request');
+var data = JSON.parse(fs.readFileSync('./putian.json'));
+var hospitals = [];
+for (var city in data) {
+    hospitals = hospitals.concat(data[city].hospital.map(function(value, index) {
+        return city + ' ' + value;
+    }));
+}
+// url=http://restapi.amap.com/v3/geocode/geo?key=你的key&address=泉州南亚医院&city=泉州
+fs.appendFileSync("putianloc.csv", 'name,lng,lat\n');
+async.eachLimit(hospitals, 10, function(hospital, cb) {
+    var uri = 'http://restapi.amap.com/v3/geocode/geo?key=你的key&address=' + hospital.split(' ')[1] + '&city=' + hospital.split(' ')[0];
+    request(encodeURI(uri), function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(response.body);
+            if (body.status === '1' && body.geocodes.length > 0) {
+                var location = body.geocodes[0].location.split(',');
+                var str = hospital.split(' ')[1] + ',' + Number(location[0]) + ',' + Number(location[1]) + '\n';
+                fs.appendFileSync("putianloc.csv", str);
+                console.log('完成' + hospital);
+            }
+        }
+        cb();
+    });
+});
