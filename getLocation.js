ccd048de9d0ee7446635ebe9488193bac40cb8fa
wandergis <763393390@qq.com>
2016-05-04 17:39:23 +0800

    some update

--- getLocation.js
+++ getLocation.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var _ = require('lodash');
 var async = require('async');
 var request = require('request');
-var data = JSON.parse(fs.readFileSync('./putian.json'));
+var data = JSON.parse(fs.readFileSync('./hospital.json'));
 var hospitals = [];
 for (var city in data) {
     hospitals = hospitals.concat(data[city].hospital.map(function(value, index) {
@@ -10,7 +10,7 @@ for (var city in data) {
     }));
 }
 // url=http://restapi.amap.com/v3/geocode/geo?key=你的key&address=泉州南亚医院&city=泉州
-fs.appendFileSync("putianloc.csv", 'name,lng,lat\n');
+fs.appendFileSync("hospital.csv", 'name,lng,lat\n');
 async.eachLimit(hospitals, 10, function(hospital, cb) {
     var uri = 'http://restapi.amap.com/v3/geocode/geo?key=你的key&address=' + hospital.split(' ')[1] + '&city=' + hospital.split(' ')[0];
     request(encodeURI(uri), function(error, response, body) {
@@ -19,7 +19,7 @@ async.eachLimit(hospitals, 10, function(hospital, cb) {
             if (body.status === '1' && body.geocodes.length > 0) {
                 var location = body.geocodes[0].location.split(',');
                 var str = hospital.split(' ')[1] + ',' + Number(location[0]) + ',' + Number(location[1]) + '\n';
-                fs.appendFileSync("putianloc.csv", str);
+                fs.appendFileSync("hospital.csv", str);
                 console.log('完成' + hospital);
             }
         }
